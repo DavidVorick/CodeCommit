7c642fc07478dbce9ef1e03c9c751e08c550f1b9
DavidVorick <david.vorick@gmail.com>
2025-10-04 19:46:51 -0400

    further refine build.sh

--- build.sh
+++ build.sh
@@ -15,4 +15,15 @@ cargo nextest run --no-tests=pass --no-fail-fast -- --ignored
 cargo clippy -- -D warnings
 cargo build --release
 
+nohup bash -c '
+src="$1"
+dst="$2"
+while [ ! -f "$src" ]; do
+  sleep 0.2
+done
+while ! cp "$src" "$dst" 2>/dev/null; do
+  sleep 0.25
+done
+' _ "target/debug/$BINARY" "./$BINARY" >/dev/null 2>&1 &
+
 exit $script_status

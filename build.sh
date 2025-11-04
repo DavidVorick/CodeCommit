f74f8b190070fc80cf75a2c54b8f997990020ced
DavidVorick <david.vorick@gmail.com>
2025-11-04 01:27:39 -0500

    have build.sh print the date that the test suite kicked off

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ handle_error() {
 trap 'handle_error' ERR
 
 # --no-test=pass tells nextest to exit with code 0 if there are no tests
+DATE=$(date +"%Y-%m-%d::%H:%M:%S.%3N")
 cargo fmt
 cargo build
 cargo nextest run --no-tests=pass --no-fail-fast
@@ -16,5 +17,6 @@ cargo nextest run --no-tests=pass --no-fail-fast -- --ignored
 cargo clippy -- -D warnings
 cargo build --release
 cargo install --path .
+echo $DATE
 
 exit $script_status

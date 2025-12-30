a8cc27b797599ff8a6a891a5830016932cbb4c75
DavidVorick <david.vorick@gmail.com>
2025-12-30 06:44:30 -0500

    finish auto-workflow for system_prompts

--- src/system_prompts/mod.rs
+++ src/system_prompts/mod.rs
@@ -5,8 +5,7 @@ pub const COMMITTING_CODE_REPAIR_QUERY: &str = include_str!("committing_code_rep
 pub const COMMITTING_CODE_EXTRA_CODE_QUERY: &str =
     include_str!("committing_code_extra_code_query.txt");
 #[allow(dead_code)]
-pub const COMMITTING_CODE_REFACTOR_QUERY: &str =
-    include_str!("committing_code_refactor_query.txt");
+pub const COMMITTING_CODE_REFACTOR_QUERY: &str = include_str!("committing_code_refactor_query.txt");
 pub const CONSISTENCY_CHECK: &str = include_str!("consistency_check.txt");
 pub const PROJECT_STRUCTURE: &str = include_str!("project_structure.txt");
 
